0fa9173c4211f31e97a86019e27afa4559572f2b
Yonglin Li <liyonglin.nju@gmail.com>
2022-12-05 17:39:02 +0800

    Update Dirichlet_bc.py

--- Dirichlet_bc.py
+++ Dirichlet_bc.py
@@ -47,71 +47,139 @@ v = TestFunction(V)
 a = inner(grad(u),grad(v))*dx + inner(u,v)*dx
 f = inner(rhsf(x),v)*dx
 
-a_mat = assemble(a).petscmat
-# a_mat.view()
-f_ = assemble(f)
+def solve_Dirichlet_v1(V, bc, a, f):
+    uh = Function(V)
+    solve(a==f, uh, bcs=bc, \
+        solver_parameters={"ksp_type": "preonly", "pc_type": "lu"}, \
+            options_prefix='solve_v1')
+    # printf('Ref. solution is')
+    # with uh.dat.vec as vec:
+    #     vec.view()
 
-uh = Function(V)
-solve(a==f, uh, bcs=bc)
-printf('Ref. solution is')
-with uh.dat.vec as vec:
-    vec.view()
+    # err = errornorm(exactu(x), uh, norm_type="H1")
+    # unorm = norm(exactu(x), norm_type="H1", mesh=mesh)
+    # # printf(f'H1 norm of u is {unorm}')
+    # printf(np.real(err/unorm))
 
-
-D = PETSc.Mat().createAIJ(a_mat.getSizes())
-rlgmap, clgmap = a_mat.getLGMap()
-D.setLGMap(rlgmap, clgmap)
-D.setUp()
-D.assemble()
-D.shift(1)
-# D.view()
+    return uh
 
 
-# Find boundary nodes
-_is_bc_nodes = bc.nodes < len(f_.dat.data)
-bc_nodes = bc.nodes[_is_bc_nodes]
-# syncprintf(f'[{rank}/{size}] nodes_local={bc_nodes}')
-for i in range(len(bc_nodes)):
-    D.setValueLocal(bc_nodes[i], bc_nodes[i], 0)
-# D.zeroRowsColumnsLocal(bc_nodes, diag=0)
-D.assemble()
-# D.view()
+def solve_Dirichlet_v2(V, bc, a, f):
+    a_mat = assemble(a).petscmat
+    # a_mat.view()
+    f_ = assemble(f)
 
-A = D.matMatMult(a_mat, D)
-A.setLGMap(rlgmap, clgmap)
-A.zeroRowsColumnsLocal(bc_nodes, diag=1)
-# A.view()
+    D = PETSc.Mat().createAIJ(a_mat.getSizes())
+    rlgmap, clgmap = a_mat.getLGMap()
+    D.setLGMap(rlgmap, clgmap)
+    D.setUp()
+    D.assemble()
+    D.shift(1)
+    # D.view()
 
-# solution on Dirichlet boundary
-b_fc = Function(V)
-bc.apply(b_fc)
+    # Find boundary nodes
+    _is_bc_nodes = bc.nodes < len(f_.dat.data)
+    bc_nodes = bc.nodes[_is_bc_nodes]
+    # syncprintf(f'[{rank}/{size}] nodes_local={bc_nodes}')
+    for i in range(len(bc_nodes)):
+        D.setValueLocal(bc_nodes[i], bc_nodes[i], 0)
+    # or use >>
+    # D.zeroRowsColumnsLocal(bc_nodes, diag=0)
+    D.assemble()
+    # D.view()
 
-# _mid = D'*K-I, where Ku=f
-_mid = D.matMult(a_mat)
-_mid.shift(-1) 
-# _mid.view()
+    A = D.matMatMult(a_mat, D)
+    A.setLGMap(rlgmap, clgmap)
+    A.zeroRowsColumnsLocal(bc_nodes, diag=1)
+    # A.view()
 
-with f_.dat.vec as f_vec:
-    with b_fc.dat.vec as ud:
-        b = ud.copy()
-        _mid.mult(ud, b)
-        b.scale(-1)
-    D.multAdd(f_vec, b, b) # b=D*f+b=D*f-D*K*ud+ud
-# b.view()
+    # solution on Dirichlet boundary
+    b_fc = Function(V)
+    bc.apply(b_fc)
 
-# KSP solver
-ksp = PETSc.KSP().create()
-ksp.setOperators(A)
-om = OptionsManager({'ksp_type': 'preonly', 'pc_type': 'lu'}, \
-    options_prefix='test')
-om.set_from_options(ksp)
-wh = Function(V)
-with om.inserted_options(), wh.dat.vec as w_vec:
-    ksp.solve(b, w_vec)
+    b, _mid = D.createVecs()
+    with f_.dat.vec as f_vec:
+        with b_fc.dat.vec as ud:
+            a_mat.mult(ud, _mid)
+            _mid.scale(-1)
+            D.mult(_mid, b)
+            b.axpy(1, ud)
+        D.multAdd(f_vec, b, b) # b=D*f+b=D*f-D*K*ud+ud
+    # b.view()
 
-syncprintf(np.allclose(wh.dat.data_ro_with_halos, uh.dat.data_ro_with_halos))
+    # KSP solver
+    ksp = PETSc.KSP().create()
+    ksp.setOperators(A)
+    om = OptionsManager({'ksp_type': 'preonly', 'pc_type': 'lu'}, \
+        options_prefix='solve_v2')
+    om.set_from_options(ksp)
+    wh = Function(V)
+    with om.inserted_options(), wh.dat.vec as w_vec:
+        ksp.solve(b, w_vec)
 
-err = errornorm(exactu(x), wh, norm_type="H1")
-unorm = norm(exactu(x), norm_type="H1", mesh=mesh)
-printf(f'H1 norm of u is {unorm}')
-printf(np.real(err/unorm))
+    uh = solve_Dirichlet_v1(V, bc, a, f)
+    syncprintf(np.allclose(wh.dat.data_ro_with_halos, uh.dat.data_ro_with_halos))
+
+    err = errornorm(exactu(x), wh, norm_type="H1")
+    unorm = norm(exactu(x), norm_type="H1", mesh=mesh)
+    # printf(f'H1 norm of u is {unorm}')
+    printf(np.real(err/unorm))
+
+
+def solve_Dirichlet_v3(V, bc, a, f):
+    a_mat = assemble(a).petscmat
+    # a_mat.view()
+    f_ = assemble(f)
+
+    # Find boundary nodes
+    _is_bc_nodes = bc.nodes < len(f_.dat.data)
+    bc_nodes = bc.nodes[_is_bc_nodes]
+
+    rlgmap, clgmap = a_mat.getLGMap()
+    B = a_mat.copy()
+    # B.setLGMap(rlgmap, clgmap)
+    B.zeroRowsColumnsLocal(bc_nodes, diag=1)
+    # B.view()
+
+    # solution on Dirichlet boundary
+    b_fc = Function(V)
+    bc.apply(b_fc)
+
+    b = B.createVecRight()
+    g_nodes = rlgmap.apply(bc_nodes)
+    with f_.dat.vec as f_vec:
+        with b_fc.dat.vec as ud:
+            a_mat.mult(ud, b)
+            b.scale(-1)
+            b.setValues(g_nodes, np.zeros(len(g_nodes)))
+            f_vec.setValues(g_nodes, np.zeros(len(g_nodes)))
+            # f_vec.setLGMap(rlgmap)
+            # b.setValuesLocal(bc_nodes, np.zeros(len(bc_nodes)))
+            # f_vec.setValuesLocal(bc_nodes, np.zeros(len(bc_nodes)))
+            b.assemble()
+            f_vec.assemble()
+            b.axpy(1, ud) 
+            b.axpy(1, f_vec) # b=D*f-D*K*ud+ud
+    # b.view()
+
+    # KSP solver
+    ksp = PETSc.KSP().create()
+    ksp.setOperators(B)
+    om = OptionsManager({'ksp_type': 'preonly', 'pc_type': 'lu'}, \
+        options_prefix='solve_v3')
+    om.set_from_options(ksp)
+    zh = Function(V)
+    with om.inserted_options(), zh.dat.vec as z_vec:
+        ksp.solve(b, z_vec)
+
+    uh = solve_Dirichlet_v1(V, bc, a, f)
+    syncprintf(np.allclose(zh.dat.data_ro_with_halos, uh.dat.data_ro_with_halos))
+
+    err = errornorm(exactu(x), zh, norm_type="H1")
+    unorm = norm(exactu(x), norm_type="H1", mesh=mesh)
+    # printf(f'H1 norm of u is {unorm}')
+    printf(np.real(err/unorm))
+
+if __name__ == '__main__':
+    solve_Dirichlet_v2(V, bc, a, f)
+    solve_Dirichlet_v3(V, bc, a, f)
